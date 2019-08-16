5f7703fcc471e902d9b0e7e8bb11a91f907261b9
w!nd <chengf_28@163.com>
2019-08-16 19:46:55 +0800

    temp update

--- framework/Router/Router.php
+++ framework/Router/Router.php
@@ -3,4 +3,20 @@
 namespace Core\Router;
 
 class Router
-{ }
+{ 
+    
+    private $collection;
+
+    private $generator;
+
+    public function __construct(\Core\Route\RouteCollection $collection, \Core\Router\RouteGenerator $generator)
+    {
+        $this->collection = $collection;
+        $this->generator  = $generator;
+    }
+
+    public function get()
+    {
+
+    }
+}
